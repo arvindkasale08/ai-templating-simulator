afc1a78725b4a1634e4e2349e27ec6988898a2ab
Arvind Kasale <arvind.kasale@appdirect.com>
2019-09-20 14:45:41 +0530

    Add iterative webclient calls

--- routes/aitemplate.js
+++ routes/aitemplate.js
@@ -59,6 +59,42 @@ router.post('/app/:appId/templates/metrics', function(req, res, next) {
   });
 });
 
+/* Paginated Response testing */
+router.get('/paginated', function(req, res, next) {
+  console.log('Request query', req.query);
+  console.log('Request params', req.params);
+  console.log('Request body', req.body);
+  console.log('Request cookies', req.cookies);
+  let page = parseInt(req.query.page) || 1;
+  console.log(`Page is ${page}`);
+  let data = [];
+  let pagination = {
+    next: page >= 3 ? null : page + 1,
+    prev: page - 1,
+    total: 10
+  }
+
+  for (i=0; i<10; i++) {
+    data.push({
+      "id": i+((page - 1 )*10),
+      "name": `User-${i+((page - 1 )*10)}`
+    })
+  }
+
+  let response = {
+    data,
+    page: pagination
+  };
+
+  console.log(response);
+  //res
+  //.json(response);
+
+  //res.status(401).json({"code": "UNAUTHORIZED"});
+
+  res.send(response);
+});
+
 
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,31 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/users/:owner/repos', function(req, res, next) {
+  let owner = req.params.owner;
+  let response = {
+    onwerName: owner,
+    repos : [
+      "repo1",
+      "repo2",
+      "repo3"
+    ]
+  }
+
+  res.json(response);
+});
+
+router.get('/users/:owner/repos/:repoName', function(req, res, next) {
+  let repoName = req.params.repoName;
+  let owner = req.params.owner;
+  let response = {
+    onwerName: owner,
+    repoName: repoName,
+    creator: owner,
+    comments: 45
+  }
+
+  res.json(response);
+});
+
 module.exports = router;
